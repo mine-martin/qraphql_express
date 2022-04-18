7775aeb7d2195ea31f5c02775d93bfd267fd36ce
mine-martin <wangondumn@gmail.com>
2022-04-18 13:19:03 +0300

    Rebuild the schema

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-const app = express();
-
-const Port = process.env.Port || 8080;
-
 // GraphQL schema
 const schema = buildSchema(`
 
@@ -18,6 +14,12 @@ const schema = buildSchema(`
 const root = {
   message: () => 'Welcome to GraphQL server with Nodejs and Express',
 };
+
+// Create an express server and a GraphQL endpoint
+
+const app = express();
+
+const Port = process.env.Port || 8080;
 
 app.use(
   '/graphql',
